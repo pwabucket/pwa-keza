30e9b540c84e7c52fc27edd18862453cc647aa88
Sadiq Salau <sadiqsalau888@gmail.com>
2025-06-22 09:52:44 +0100

    Added Utils

---
+++ src/lib/utils.js
@@ -0,0 +1,14 @@
+import clsx from "clsx";
+import repeatElement from "repeat-element";
+import { Fragment, createElement } from "react";
+import { twMerge } from "tailwind-merge";
+
+export function repeatComponent(component, times = 1) {
+  return repeatElement(undefined, times).map((_, i) =>
+    createElement(Fragment, { key: i, children: component })
+  );
+}
+
+export function cn(...inputs) {
+  return twMerge(clsx(inputs));
+}
