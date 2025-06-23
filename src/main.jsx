bee15534d187f735df08cf6ba5df36e070edefdc
Sadiq Salau <sadiqsalau888@gmail.com>
2025-06-23 16:58:13 +0100

    Added PWA assets

--- src/main.jsx
+++ src/main.jsx
@@ -1,11 +1,14 @@
-import "./index.css";
 import "./lib/polyfill.js";
 
 import { BrowserRouter } from "react-router";
 import { StrictMode } from "react";
 import { createRoot } from "react-dom/client";
+import { registerSW } from "virtual:pwa-register";
 
 import App from "./App.jsx";
+
+/** Register Service Worker */
+registerSW({ immediate: true });
 
 createRoot(document.getElementById("root")).render(
   <StrictMode>
--- vite.config.js
+++ vite.config.js
@@ -1,8 +1,80 @@
+import path from "path";
+import process from "process";
 import react from "@vitejs/plugin-react-swc";
 import tailwindcss from "@tailwindcss/vite";
+import { ViteEjsPlugin } from "vite-plugin-ejs";
+import { VitePWA } from "vite-plugin-pwa";
 import { defineConfig } from "vite";
+import { fileURLToPath } from "url";
+import { loadEnv } from "vite";
+
+const __filename = fileURLToPath(import.meta.url);
+const __dirname = path.dirname(__filename);
 
 // https://vite.dev/config/
-export default defineConfig({
-  plugins: [tailwindcss(), react()],
+export default defineConfig(({ mode }) => {
+  /** Env */
+  const env = loadEnv(mode, process.cwd());
+
+  return {
+    plugins: [
+      /** Plugins */
+      VitePWA({
+        registerType: "autoUpdate",
+        workbox: {
+          globPatterns: ["**/*.*"],
+          maximumFileSizeToCacheInBytes: 5 * 1024 ** 2,
+        },
+        manifest: {
+          name: env.VITE_APP_NAME,
+          short_name: env.VITE_APP_NAME,
+          description: env.VITE_APP_DESCRIPTION,
+          theme_color: "#ffffff",
+          icons: [
+            {
+              src: "pwa-64x64.png",
+              sizes: "64x64",
+              type: "image/png",
+            },
+            {
+              src: "pwa-192x192.png",
+              sizes: "192x192",
+              type: "image/png",
+            },
+            {
+              src: "pwa-512x512.png",
+              sizes: "512x512",
+              type: "image/png",
+            },
+            {
+              src: "maskable-icon-512x512.png",
+              sizes: "512x512",
+              type: "image/png",
+              purpose: "maskable",
+            },
+          ],
+          screenshots: [
+            {
+              src: "screenshot-mobile-1.jpg",
+              sizes: "1080x1920",
+              type: "image/jpg",
+            },
+            {
+              src: "screenshot-mobile-2.jpg",
+              sizes: "1080x1920",
+              type: "image/jpg",
+            },
+            {
+              src: "screenshot-mobile-3.jpg",
+              sizes: "1080x1920",
+              type: "image/jpg",
+            },
+          ],
+        },
+      }),
+      ViteEjsPlugin(env),
+      tailwindcss(),
+      react(),
+    ],
+  };
 });
