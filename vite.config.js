d1cc709c3f87e41ba5cf5a90a27f26b2d770880d
Sadiq Salau <sadiqsalau888@gmail.com>
2025-06-24 09:21:56 +0100

    Updated Background Color

--- vite.config.js
+++ vite.config.js
@@ -31,6 +31,7 @@ export default defineConfig(({ mode }) => {
           short_name: env.VITE_APP_NAME,
           description: env.VITE_APP_DESCRIPTION,
           theme_color: "#262626",
+          background_color: "#262626",
           icons: [
             {
               src: "pwa-64x64.png",
