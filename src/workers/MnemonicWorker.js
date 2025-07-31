54f8aab98ef1c5602beb97ab891403ec12261bff
Sadiq Salau <sadiqsalau888@gmail.com>
2025-07-31 11:16:31 +0100

    chore: Removed logging

--- src/workers/MnemonicWorker.js
+++ src/workers/MnemonicWorker.js
@@ -5,7 +5,6 @@ import { generateMnemonic } from "bip39";
 import { createWalletWorker } from "./createWalletWorker";
 
 createWalletWorker(async (strength) => {
-  console.log(strength);
   let mnemonic = generateMnemonic(strength === 12 ? 128 : 256);
 
   return {
